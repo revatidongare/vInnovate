844e37c54603c1942d3237c6e500f106385235d5
rohan khandelwal <rohankhandelwal172001@gmail.com>
2020-01-14 21:18:29 +0530

    head change

--- index.php
+++ index.php
@@ -2,30 +2,8 @@
 <html lang="en">
 
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<title>vInnovate Technologies - HomePage</title>
-	<link rel="icon" href="img/vNew.png" sizes="16*16">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-	<link rel="stylesheet" type="text/css" href="css/theme-styles.css">
-	<link rel="stylesheet" type="text/css" href="css/blocks.css">
-	<link rel="stylesheet" type="text/css" href="css/widgets.css">
-	<!-- <link href="https://fonts.googleapis.com/css?family=Slabo+27px&display=swap" rel="stylesheet"> -->
-
-
-	<!--External fonts-->
-
-	<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700,900" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-
-	<!-- Styles for Plugins -->
-	<link rel="stylesheet" type="text/css" href="css/swiper.min.css">
-
-</head>
 <body class="crumina-grid">
+	<?php include 'head.php';?>
 
 <!-- Header -->
 
